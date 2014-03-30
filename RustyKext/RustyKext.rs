163d9d485899a417213744601f9efa72f8693202
Phil Jordan <phil@philjordan.eu>
2014-03-30 19:31:45 +0900

    Compile rust to bytecode with rustc and then compile that to object code with clang. This avoids the need to annotate every function with #[no_split_stack]

--- RustyKext/RustyKext.rs
+++ RustyKext/RustyKext.rs
@@ -9,14 +9,12 @@ extern "C" {
 	pub fn IOLog(fmt: *u8);
 }
 
-#[no_split_stack]
 unsafe fn print(s: &str) {
 	let (ptr, _): (*u8, uint) = transmute(s);
 	IOLog(ptr);
 }
 
 #[no_mangle]
-#[no_split_stack]
 pub unsafe fn rust_main() {
 	print("hello from rust\n\0");
 }
